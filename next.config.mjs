3fa11548050fdb613315006526f97b26577d65d8
Sidali <saadisidali010@gmail.com>
2024-10-30 04:10:31 +0100

    Remove unnecessary rewrites from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output :'standalone',
-    rewrites: async () => {
-        return [
-            {
-                source: '/:path*',
-                destination: '/:path*',
-            },
-        ];
-    },
+    
 };
 
 export default nextConfig;
