2e8be1872daf0d7086f1e1c5b809ee83a6b5fcbe
Sidali <saadisidali010@gmail.com>
2024-11-06 03:46:18 +0100

    Refactor PostHog provider initialization: move initialization to window check and update person_profiles setting

--- src/providers/posthog.provider.tsx
+++ src/providers/posthog.provider.tsx
@@ -3,16 +3,12 @@ import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import { useEffect } from "react";
 
-export function PHProvider({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-      person_profiles: "identified_only",
-      capture_pageview: true, // Disable automatic pageview capture, as we capture manually
-    });
-
-    posthog.capture("$pageview");
-  }, []);
-
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "always", // or 'always' to create profiles for anonymous users as well
+  });
+}
+export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
