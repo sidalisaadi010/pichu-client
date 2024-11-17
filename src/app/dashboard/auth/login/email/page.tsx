cec2441e8ab5b5a0d4c6b5eb4a1a4a615919c1e5
Sidali <saadisidali010@gmail.com>
2024-11-17 22:31:45 +0100

    Add logging for token in EmailLoginCallback to assist with debugging

--- src/app/dashboard/auth/login/email/page.tsx
+++ src/app/dashboard/auth/login/email/page.tsx
@@ -46,6 +46,8 @@ export default function EmailLoginCallback({
 
   useEffect(() => {
     const authenticate = async () => {
+      console.log(token);
+
       if (!token) {
         console.log(token);
 
